4f4b132c298ab037e131336ed9b9979748e107e8
Julia <z3211232001@gmail.com>
2025-01-20 16:18:21 -0800

    finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
-class Calculator {
+const buttonCalc = document.querySelector('#calculateBtn')
 
+class Calculator {
+  constructor() {
+    this.num1 = document.querySelector('#num1')
+    this.num2 = document.querySelector('#num2')
+    this.result = document.querySelector('#result')
+    this.operation = document.querySelector('#operation')
+
+    // document.querySelector('#calculateBtn').addEventListener('click', () => {
+    //     this.compute()
+    //   })
+  }
+
+  compute() {
+    if (this.operation.value === "add") {
+      this.add()
+    } else if (this.operation.value === "subtract") {
+      this.subtract()
+    } else if (this.operation.value === "multiply") {
+      this.multiply()
+    } else if (this.operation.value === "divide") {
+      this.divide()
+    }
+  }
+
+  add() {
+    this.result.innerHTML = Number(this.num1.value) + Number(this.num2.value)
+  }
+
+  subtract() {
+    this.result.innerHTML = Number(this.num1.value) - Number(this.num2.value)
+  }
+
+  multiply() {
+    this.result.innerHTML = Number(this.num1.value) * Number(this.num2.value)
+  }
+
+  divide() {
+    this.result.innerHTML = Number(this.num1.value) / Number(this.num2.value)
+  }
 }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const calculator = new Calculator()
+// })
+
+buttonCalc.addEventListener('click', () => {
+  const calculator = new Calculator()
+  calculator.compute()
+})
